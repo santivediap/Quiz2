26099a725b6cdee721d17f7c71becc77577444da
SVega9848 <santivediap@gmail.com>
2023-05-25 15:56:08 +0200

    Fix DOM errors

--- script.js
+++ script.js
@@ -28,73 +28,76 @@ async function getQuestions() {
 
 async function paintQuestions() {
 
-    try {
-    let val = questionsList
-        val.forEach((ele, index) => {
-
-            // Pushea la opcion correcta al resto de opciones
-            ele.incorrect_answers.push(ele.correct_answer)
-
-            // Crear nuevas opciones
-            let questionOptions = []
-
-            while(questionOptions.length < 4) {
-                let randomOption = Math.floor(Math.random() * ele.incorrect_answers.length)
-                questionOptions.push(ele.incorrect_answers[randomOption])
-                ele.incorrect_answers.splice(randomOption, 1)
-            }
-
-            let questionContainer = document.createElement("div")
-            questionContainer.className = "hide"
-            questionContainer.setAttribute("id", "question")
-
-            let questionsh3 = document.createElement("h3")
-            let questionsh3text = document.createTextNode(`${index+1}. ${ele.question}`)
-            questionsh3.appendChild(questionsh3text)
-
-            let questionArticle = document.createElement("article")
-            questionArticle.appendChild(questionsh3)
+    if(document.querySelector("#quiz")) {
+        try {
+            let val = questionsList
+                val.forEach((ele, index) => {
+        
+                    // Pushea la opcion correcta al resto de opciones
+                    ele.incorrect_answers.push(ele.correct_answer)
+        
+                    // Crear nuevas opciones
+                    let questionOptions = []
+        
+                    while(questionOptions.length < 4) {
+                        let randomOption = Math.floor(Math.random() * ele.incorrect_answers.length)
+                        questionOptions.push(ele.incorrect_answers[randomOption])
+                        ele.incorrect_answers.splice(randomOption, 1)
+                    }
+        
+                    let questionContainer = document.createElement("div")
+                    questionContainer.className = "hide"
+                    questionContainer.setAttribute("id", "question")
+        
+                    let questionsh3 = document.createElement("h3")
+                    let questionsh3text = document.createTextNode(`${index+1}. ${ele.question}`)
+                    questionsh3.appendChild(questionsh3text)
+        
+                    let questionArticle = document.createElement("article")
+                    questionArticle.appendChild(questionsh3)
+                    
+                    // Crear elementos de opciones
+                    let optionsContainer = document.createElement("div")
+                    optionsContainer.className = "options-container"
+        
+                    questionOptions.forEach(element => {
+                        let optionLabel = document.createElement("label")
+                        optionLabel.className = "option"
+        
+                        let optionParagraph = document.createElement("p")
+                        let optionParagraphText = document.createTextNode(element)
+                        optionParagraph.appendChild(optionParagraphText)
+        
+                        let optionInput = document.createElement("input")
+                        optionInput.setAttribute("type", "radio")
+                        optionInput.setAttribute("name", index)
+                        optionInput.setAttribute("value", element)
+                        
+        
+                        optionLabel.appendChild(optionParagraph)
+                        optionLabel.appendChild(optionInput)
+        
+                        optionsContainer.appendChild(optionLabel)
+                    });
+        
+                    questionContainer.appendChild(questionArticle)
+                    questionContainer.appendChild(optionsContainer)
+        
+                    document.querySelector("#quiz").appendChild(questionContainer)
+        
+                })
+                    // Creación del botón
+                    let submitButton = document.createElement("button");
+                    submitButton.setAttribute("type", "submit");
+                    submitButton.className = "button-container";
+                    submitButton.setAttribute("id", "submit-quiz")
+                    submitButton.textContent = "Siguiente";
             
-            // Crear elementos de opciones
-            let optionsContainer = document.createElement("div")
-            optionsContainer.className = "options-container"
-
-            questionOptions.forEach(element => {
-                let optionLabel = document.createElement("label")
-                optionLabel.className = "option"
-
-                let optionParagraph = document.createElement("p")
-                let optionParagraphText = document.createTextNode(element)
-                optionParagraph.appendChild(optionParagraphText)
-
-                let optionInput = document.createElement("input")
-                optionInput.setAttribute("type", "radio")
-                optionInput.setAttribute("name", index)
-                optionInput.setAttribute("value", element)
-                
-
-                optionLabel.appendChild(optionParagraph)
-                optionLabel.appendChild(optionInput)
-
-                optionsContainer.appendChild(optionLabel)
-            });
-
-            questionContainer.appendChild(questionArticle)
-            questionContainer.appendChild(optionsContainer)
-
-            document.querySelector("#quiz").appendChild(questionContainer)
-
-        })
-            // Creación del botón
-            let submitButton = document.createElement("button");
-            submitButton.setAttribute("type", "submit");
-            submitButton.className = "button-container";
-            submitButton.textContent = "Siguiente";
-    
-            document.querySelector("#quiz").appendChild(submitButton);
-            showQuestion()
-    } catch (error){
-        console.log(`ERROR! -> ${error}`);
+                    document.querySelector("#quiz").appendChild(submitButton);
+                    showQuestion()
+            } catch (error){
+                console.log(`ERROR! -> ${error}`);
+            }
     }
     }
 
@@ -117,6 +120,20 @@ function showQuestion() {
         existingResults.push(playerResults)
 
         localStorage.results = JSON.stringify(existingResults)
+
+        // Borrar boton del formulario
+
+        document.querySelector("#submit-quiz").remove()
+
+        // Añadir nuevo anchor para enviar a pagina de resultados
+
+        let resultsButton = document.createElement("a")
+        resultsButton.setAttribute("href", "results.html")
+        resultsButton.setAttribute("id", "results-anchor")
+        resultsButton.textContent = "Resultados"
+        resultsButton.className = "button-container"
+
+        document.querySelector("#quiz").appendChild(resultsButton)
     }
 }
 
@@ -125,44 +142,41 @@ function showQuestion() {
 getQuestions().then(val => {
     questionsList = val.results
     paintQuestions()
+
+    if(document.querySelector(".ct-chart")) {
+        paintGraph()
+    }
 })
 
 // VALIDACION DEL FORMULARIO
 
-document.addEventListener("DOMContentLoaded", function() {
-
-    document.querySelector("#quiz").addEventListener("submit", function (event) {
-        event.preventDefault();
+    if(document.querySelector("#quiz")) {
+        document.querySelector("#quiz").addEventListener("submit", function (event) {
+            event.preventDefault();
+        
+            let selectedOptions = document.querySelectorAll(".options-container input:checked");
+        
+            let correctCount = 0;
+            let incorrectCount = 0;
+        
+            selectedOptions.forEach((option) => {
+              let questionIndex = option.getAttribute("name");
+              let question = document.querySelectorAll(".hide")[questionIndex];
+        
+            if (option.value == questionsList[questionIndex].correct_answer) {
+                    correctCount++;
+                } else {
+                    incorrectCount++;
+                }
     
-        let selectedOptions = document.querySelectorAll(".options-container input:checked");
-    
-        let correctCount = 0;
-        let incorrectCount = 0;
-    
-        selectedOptions.forEach((option) => {
-          let questionIndex = option.getAttribute("name");
-          let question = document.querySelectorAll(".hide")[questionIndex];
-    
-        if (option.value == questionsList[questionIndex].correct_answer) {
-                correctCount++;
-            } else {
-                incorrectCount++;
-            }
-
-            countRightAnswers = correctCount
-            countWrongAnswers = incorrectCount
-        console.log("Respuestas correctas: " + correctCount);
-        console.log("Respuestas incorrectas: " + incorrectCount);
-        })
-        showQuestion()
-      });
-});
+                countRightAnswers = correctCount
+                countWrongAnswers = incorrectCount
+            })
+            showQuestion()
+          });
+    }
 
 // Sacar informacion de partidas de LocalStorage y pintar Grafica
-
-document.addEventListener("DOMContentLoaded", function() {
-    paintGraph();
-  });
 
 async function paintGraph () { 
 
@@ -178,7 +192,7 @@ const getGamesData = JSON.parse(localStorage.getItem('results'));
   
     dateData.push(date);
     markData.push(mark);
-} console.log(dateData, markData)
+}
   
 new Chartist.Bar('.ct-chart', {
     labels: dateData,
